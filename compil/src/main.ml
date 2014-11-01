205136c1e45da3ade8b3ac072bce41f9bffb73d4
Valentin Lorentz <progval@progval.net>
2014-11-01 14:26:25 +0100

    [Compil] Show a nice error if the number of arguments is not what is expected.

--- compil/src/main.ml
+++ compil/src/main.ml
@@ -1,5 +1,8 @@
 let _ =
-  let input = open_in Sys.argv.(1) in
-  let buf = Lexing.from_channel input in
-  let s = Descr.print_garment (Descr_parser.main Descr_lexer.token buf) in
-  print_string s
+  if Array.length Sys.argv != 2 then
+    print_string "Usage: ./main.native filename.tricot\n"
+  else
+    let input = open_in Sys.argv.(1) in
+    let buf = Lexing.from_channel input in
+    let s = Descr.print_garment (Descr_parser.main Descr_lexer.token buf) in
+    print_string s
