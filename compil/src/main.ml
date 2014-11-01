8a79b9741e50136f0154c62354722d7e6e455037
Valentin Lorentz <progval@progval.net>
2014-11-01 14:29:29 +0100

    [Compil] Show nice error if the given filename does not exist.

--- compil/src/main.ml
+++ compil/src/main.ml
@@ -1,6 +1,8 @@
 let _ =
   if Array.length Sys.argv != 2 then
     print_string "Usage: ./main.native filename.tricot\n"
+  else if not (Sys.file_exists Sys.argv.(1)) then
+    print_string "File does not exist.\n"
   else
     let input = open_in Sys.argv.(1) in
     let buf = Lexing.from_channel input in
