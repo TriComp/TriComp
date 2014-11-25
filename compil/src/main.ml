972b8eda111ae8709ba7fe02ee99554393d8dd37
Maxime Lesourd <maxime.lesourd@ens-lyon.fr>
2014-11-25 21:38:20 +0100

    Arguments handling

--- compil/src/main.ml
+++ compil/src/main.ml
@@ -1,6 +1,37 @@
 open Core.Std
 
+let parse input () =
+    ( match input with
+        `Stdin -> stdin
+      | `File f ->
+        try In_channel.create f with
+        | Sys_error s ->
+          eprintf "Invalid source : %s\n%!" s;
+          exit 1 )
+    |> Lexing.from_channel
+    |> Descr_parser.main Descr_lexer.token
+    |> Descr.print_garment
+    |> printf "%s%!"
+
+
+
+let input =
+  Command.Spec.Arg_type.create
+    (function
+        "-" -> `Stdin
+      | filename ->
+        match Sys.is_file filename with
+          `Yes -> `File filename
+        | _ ->
+          eprintf "'%s' is not a regular file.\n%!" filename;
+          exit 1
+    )
+
+let command = Command.basic
+    ~summary:"Generates a sequence of instructions from a .tricot file"
+    ~readme:(const "")
+    Command.Spec.(empty +> anon ("input" %: input))
+    parse
+
 let _ =
-  let input = open_in Sys.argv.(1) in
-  let buf = Lexing.from_channel input in
-  Descr_parser.main Descr_lexer.token buf |> Descr.print_garment |> print_string
+  Command.run command
